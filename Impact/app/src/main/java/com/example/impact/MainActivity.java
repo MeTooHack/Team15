b4141f517784047731c48aab55b226bcfdd030c3
Lisa Larsson <lislar@student.chalmers.se>
2018-02-03 14:26:32 +0100

    situations started

--- Impact/app/src/main/java/com/example/impact/MainActivity.java
+++ Impact/app/src/main/java/com/example/impact/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.impact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_situation);
     }
+
 }
--- Impact/app/src/main/java/com/example/impact/SituationActivity.java
+++ Impact/app/src/main/java/com/example/impact/SituationActivity.java
@@ -1,7 +1,9 @@
 package com.example.impact;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SituationActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SituationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_situation);
+    }
+
+    //For close button
+    public void close(View view) {
+        super.onBackPressed();
     }
 }
