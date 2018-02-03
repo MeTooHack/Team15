5e13e447d430aecf0af0481715a3aba816dd4bc3
rebeccafinne <rebecca@finne.se>
2018-02-03 14:07:56 +0100

    make impact layout done

--- Impact/app/src/main/java/com/example/impact/MainActivity.java
+++ Impact/app/src/main/java/com/example/impact/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.impact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tafs1;
+    private Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.make_impact_layout);
+
+        intent = new Intent(this, SituationActivity.class);
+
+        tafs1 = (TextView) findViewById(R.id.tafs1);
+        tafs1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tafsSituation(v);
+            }
+        });
+    }
+
+
+    public void tafsSituation(View v){
+        startActivity(intent);
     }
 }
