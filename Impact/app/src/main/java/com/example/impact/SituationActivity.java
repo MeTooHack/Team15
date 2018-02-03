6c9167a85c1b3553d0e3f91477fb5c44ac629c09
rebeccafinne <rebecca@finne.se>
2018-02-03 10:38:39 +0100

    Layouts

---
+++ Impact/app/src/main/java/com/example/impact/SituationActivity.java
@@ -0,0 +1,13 @@
+package com.example.impact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SituationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_situation);
+    }
+}
