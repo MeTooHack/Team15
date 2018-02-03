a02dc6845d05181595d951d4a7be33e931b4a4dd
MajaNyberg <marnybe@student.chalmers.se>
2018-02-03 14:00:54 +0100

    add bottom navigation bar

--- Impact/app/src/main/java/com/example/impact/MainActivity.java
+++ Impact/app/src/main/java/com/example/impact/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.impact;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Action Home clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_facts:
+                        Toast.makeText(MainActivity.this, "Action Facts clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_makeimpact:
+                        Toast.makeText(MainActivity.this, "Action Make Impact clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_practice:
+                        Toast.makeText(MainActivity.this, "Action Practice clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
